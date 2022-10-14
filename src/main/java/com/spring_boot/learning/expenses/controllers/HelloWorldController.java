a90b35b1bda8902a0dd023a512ca73ac066a82a4
Łukasz Mrugała <lumr@odoo.com>
2022-10-14 09:48:33 +0200

    Path Variable used

--- src/main/java/com/spring_boot/learning/expenses/controllers/HelloWorldController.java
+++ src/main/java/com/spring_boot/learning/expenses/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.spring_boot.learning.expenses.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring_boot.learning.expenses.beans.MessageBean;
@@ -16,5 +17,10 @@ public class HelloWorldController {
     @GetMapping("/hello-bean")
     public MessageBean helloWorldBean() {
         return new MessageBean("Hello World");
+    }
+
+    @GetMapping("/hello-path/{name}")
+    public MessageBean helloPathBean(@PathVariable("name") String nameInPath) {
+        return new MessageBean(String.format("Hello, %s", nameInPath));
     }
 }
